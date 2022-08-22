a184927d08336b6c9c3b32a008c425052a7af833
Sally-maker <gabrielgbbarbosa624@gmail.com>
2022-08-22 20:09:41 -0300

    Adjustment Colors

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,7 +7,7 @@ export function SideBar() {
     const { data } = useGetLessonsQuery()
     
     return (
-        <aside className="w-[640px] overflow-y-hidden bg-gray-700 p-6 border-l border-gray-600">
+        <aside className="w-[425px] overflow-y-hidden bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
                 Cronograma de aulas
             </span>
