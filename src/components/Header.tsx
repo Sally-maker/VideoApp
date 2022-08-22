2d7da89192244188f3a60e8590a6473598ec66b5
Sally-maker <gabrielgbbarbosa624@gmail.com>
2022-08-22 20:12:49 -0300

    Adjustment Colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ import { ToggleContext } from "../contexts/ToggleContext";
 export function Header() {
  const {isOpenToggle, setIsOpenToggle} =  useContext(ToggleContext)
     return (
-       <header className="max-w-[640px] py-5 bg-gray-700 border-b flex flex-col border-gray-600 p-4 pl-5">
+       <header className="max-w-[425px] py-5 bg-gray-700 border-b flex flex-col border-gray-600 p-4 pl-5">
          <div className="max-w-full flex justify-end ">
            <img src="/src/assets/Logo.png" alt="" className="h-12 rounded-2xl" />
          </div>
            <>
-              <nav className={`max-w-[640px]`}>
+              <nav className={`max-w-[425px]`}>
                 <button  onClick={() => setIsOpenToggle(!isOpenToggle)} className={`flex justify-start items-center duration-500`}>
                     {isOpenToggle ? <List size={24}/> : <X size={24}/>}
                 </button>
