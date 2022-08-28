15807892e2bef4479517c4ca5188b51eed67cba0
Sally-maker <gabrielgbbarbosa624@gmail.com>
2022-08-27 22:24:38 -0300

    resolve errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { client } from './lib/apollo'
 
 import './styles/global.css'
 
